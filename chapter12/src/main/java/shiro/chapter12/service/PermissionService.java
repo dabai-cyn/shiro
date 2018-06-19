dbfaf5138ac38a7b4f5f9edc8fe60acb905d54a3
dabai-cyn <599283258@qq.com>
2018-06-19 17:48:30 +0800

    add

---
+++ chapter12/src/main/java/shiro/chapter12/service/PermissionService.java
@@ -0,0 +1,13 @@
+package shiro.chapter12.service;
+
+import shiro.chapter12.entity.Permission;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+public interface PermissionService {
+    public Permission createPermission(Permission permission);
+    public void deletePermission(Long permissionId);
+}
